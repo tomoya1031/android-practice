6ec54db80fb8b77532e749bc172a475f6a893dca
tomoya1031 <rowingwant@yahoo.co.jp>
2022-03-04 01:17:50 +0900

    フィールド名取得参考

--- app/src/main/java/com/example/myapplication/utils/FailUtil.java
+++ app/src/main/java/com/example/myapplication/utils/FailUtil.java
@@ -1,9 +1,12 @@
 package com.example.myapplication.utils;
 
+import com.example.myapplication.dto.PriceDto;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.util.Properties;
 
 public class FailUtil {
@@ -30,6 +33,11 @@ public class FailUtil {
     public void set(String key, String value){
         prop.setProperty(key, value);
         try {
+            //フィールド名取得処理参考
+            Field[] fields = PriceDto.class.getDeclaredFields();
+            for(Field f: fields){
+                System.out.println(f.getName());
+            }
             prop.store(new FileOutputStream(FILE_URL), "Comments");
         } catch (IOException e) {
             e.printStackTrace();
